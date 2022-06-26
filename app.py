d8204ea1bcdd526625ca157b7075a9511c7767b4
akshay <akshay22.11.1995@gmail.com>
2022-06-26 17:11:19 +0530

    not working

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return("Starting the project")
+    return("Starting the project again")
 
 if __name__=="main":
     app.run(debug=True)
-
