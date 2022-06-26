699e98dd6692137c1c532c8e5840fc4f7247a774
akshay <akshay22.11.1995@gmail.com>
2022-06-26 17:17:30 +0530

    working

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return("Starting the project again")
+    return("Confirming for the last time")
 
 if __name__=="main":
     app.run(debug=True)
